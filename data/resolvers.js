548570c792f57b3f663353ffd075e3a6e873e89d
Blake Noll <blakenoll@bnollMBP15-MKT.local>
2018-03-01 10:06:33 -0800

    initial commit

---
+++ data/resolvers.js
@@ -0,0 +1,27 @@
+import { Author, Post } from './connectors';
+
+const resolvers = {
+  Query: {
+    author(_, args) {
+      return Author.find({ where: args });
+    },
+    allAuthors() {
+      return Author.findAll();
+    },
+  },
+  Author: {
+    posts(author) {
+      return author.getPosts();
+    }
+  },
+  Post: {
+    author(post) {
+      return post.getAuthor();
+    },
+    views(post) {
+      return Post.findOne({ postId: post.id }).then(view => post.views);
+    }
+  }
+};
+
+export default resolvers;
